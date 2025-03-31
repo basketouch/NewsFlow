46ceb419619abd627676cb5a62d9f9f124c3c166
Jorge Lorenzo <app@basketouch.com>
2025-03-31 19:36:05 +0200

    Initial Commit

---
+++ NewsFlow 3.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsFlow 3.0
+//
+//  Created by Jorge Lorenzo on 31/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsFlow 3.0/NewsFlow_3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFlow_3_0App.swift
+//  NewsFlow 3.0
+//
+//  Created by Jorge Lorenzo on 31/3/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsFlow_3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
